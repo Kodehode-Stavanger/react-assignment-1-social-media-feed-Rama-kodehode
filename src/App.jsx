2df193c622a1555041d957931f518b2c1e0ab131
Rama Kodehode <rama.kodehode@gmail.com>
2024-03-13 13:58:33 +0100

    First deliver

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
-function App() {}
+import Card from "./Card/Card";
+// import "./Card.css";
+
+function App() {
+  return (
+    <>
+      <div className="card-container">
+        <Card />
+      </div>
+    </>
+  );
+}
 
 export default App;
